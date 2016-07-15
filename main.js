b60b2eb4d9ee33aebc195e2c1e735d626f74b416
Trouble_SUM <Cellophan_tape@yahoo.co.jp>
2016-07-16 02:40:07 +0900

    update js ver. 0.0

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-enchant();
+  enchant();
 
 window.onload = function() {
 	var game = new Core(320,480); 
 	//GameInit
+	
+	//////////////////////////////////////////////////
+	//Image
 	game.preload('assets/syamu.png');
 	game.preload('assets/bullet.png');
 	game.preload('assets/musyokuorig.png');
 	game.preload('assets/musyokured.png');
 	game.preload('assets/musyokublue.png');
-	
+	//////////////////////////////////////////////////
+	//Sound
+	game.preload('assets/tropicalnojob.mp3');
+	game.preload('assets/taima.mp3');
+	game.preload('assets/taimasp.mp3');
+	game.preload('assets/taimasp2.mp3');
+	//////////////////////////////////////////////////
+	//Game Settings
 	game.fps = 30;
 	game.rootScene.backgroundColor = '00C3FF';
 	game.scale = 1;
@@ -18,58 +28,134 @@ window.onload = function() {
 	var MOVE_RANGE_X = game.width - 32;
 	var MOVE_RANGE_Y = game.height - 32;
 	
-	var left	 = 0;
-	var right	= MOVE_RANGE_X;
+	var left = 0;
+	var right = MOVE_RANGE_X;
 	var top	= 0;
 	var bottom = MOVE_RANGE_Y;
-
+		
 	var start = new Scene();
 	var end = new Scene();
+
+	//Set BGM
+	var bgm = game.assets['assets/tropicalnojob.mp3'];
+
+	/*function sleep(){
+		var sleeptime = Math.floor(game.frame/game.fps);
+		var nowtime = 0;
+		var counter = 0;
+		
+		console.log(sleeptime);
+		
+		while(counter < 10) {
+			nowtime = Math.floor(game.frame/game.fps);
+			counter = nowtime-sleeptime;
+		}
+	}*/
+
+	//taima
+	function taima(){
+		
+		//Set BGM
+		var bgm = game.assets['assets/tropicalnojob.mp3'];
 	
+		//Set SE
+		var taimasp = game.assets['assets/taimasp.mp3'];
+		var taimasp2 = game.assets['assets/taimasp2.mp3'];
+		//var taima = game.assets['assets/taima.mp3'];
+		
+		//Main
+		var nowtime = 1;
+		game.pushScene(end);
+		bgm.stop();
+		
+
+		///////////////////////////////////////////////////
+		game.addEventListener('enterframe',function() {
+
+			if(game.frame % game.fps == 0){
+				switch(nowtime){
+					case 1:
+						game.rootScene.backgroundColor = "#5996ff";
+						taimasp.play();
+						nowtime++;
+						break;
+					case 2:
+						game.rootScene.backgroundColor = "#f5ff59";
+						taimasp.play();
+						nowtime++;
+						break;
+					case 3:
+						game.rootScene.backgroundColor = "#67ff59";
+						taimasp.play();
+						nowtime++;
+						break;
+					case 4:
+						game.rootScene.backgroundColor = "#ff5959";
+						taimasp.play();
+						nowtime++;
+						break;
+					case 5:
+						game.rootScene.backgroundColor = "#424242";
+						taimasp2.play();
+						nowtime++;
+						break;
+				}
+			}
+		})
+
+	}
+		
+	//StartMessage
+	start.backgroundColor = "#FF9999";
+	var startMsg = new Label("Syamu Shooting");
+	startMsg.x = 50;
+	startMsg.y = 100;
+	startMsg.font = "normal normal 32px/1.0 monospace";
+	startMsg.color = "#FFFFFF";
+	start.addChild(startMsg);
+	var setsumeiMsg = new Label("Press Space Key");
+	setsumeiMsg.x = 100;
+	setsumeiMsg.y = 150;
+	setsumeiMsg.font = "normal normal 16px/1.0 monospace";
+	setsumeiMsg.color = "#FFFFFF";
+	start.addChild(setsumeiMsg);
+	var versionMsg = new Label("ver. 0.0");
+	versionMsg.x = 250;
+	versionMsg.y = 440;
+	versionMsg.font = "normal normal 12px/1.0 monospace";
+	versionMsg.color = "#FFFFFF";
+	start.addChild(versionMsg);
+	
+	
+	//EndMessage
+	var endMsg = new Label("GameOver");
+	var endScore = new Label("SCORE: "+game.score);
+	var endReMsg = new Label("Press F5 Key");
+	endMsg.x = 100;
+	endMsg.y = 200;
+	endMsg.font = "normal normal 32px/1.0 monospace";
+	endMsg.color = "#FFFFFF";
+	end.addChild(endMsg);
+	endScore.x = 130;
+	endScore.y = 240;
+	endScore.font = "normal normal 16px/1.0 monospace";
+	endScore.color = "#FFFFFF";
+	end.addChild(endScore);
+	endReMsg.x = 130;
+	endReMsg.y = 280;
+	endReMsg.font = "normal normal 12px/1.0 monospace";
+	endReMsg.color = "#FFFFFF";
+	end.addChild(endReMsg);
+	
+	
+	//GameStart
 	game.onload = function() {
-	
+		
 		game.keybind(' '.charCodeAt(0), 's');
 		game.keybind(90, 'a');
 		
-		start.backgroundColor = "#FF9999";
-		// シーンにメッセージを登録
-		var startMsg = new Label("Syamu Shooting");
-		startMsg.x = 50;
-		startMsg.y = 100;
-		startMsg.font = "normal normal 32px/1.0 monospace";
-		startMsg.color = "#FFFFFF";
-		start.addChild(startMsg);
-		var setsumeiMsg = new Label("Press Space Key");
-		setsumeiMsg.x = 100;
-		setsumeiMsg.y = 150;
-		setsumeiMsg.font = "normal normal 16px/1.0 monospace";
-		setsumeiMsg.color = "#FFFFFF";
-		start.addChild(setsumeiMsg);
-		var versionMsg = new Label("ver.0");
-		versionMsg.x = 250;
-		versionMsg.y = 440;
-		versionMsg.font = "normal normal 12px/1.0 monospace";
-		versionMsg.color = "#FFFFFF";
-		start.addChild(versionMsg);
-		
-		var endMsg = new Label("GameOver");
-		var endScore = new Label("SCORE: "+ game.score);
-		var endReMsg = new Label("Press F5 Key");
-		endMsg.x = 100;
-		endMsg.y = 200;
-		endMsg.font = "normal normal 32px/1.0 monospace";
-		endMsg.color = "#FFFFFF";
-		end.addChild(endMsg);
-		endScore.x = 130;
-		endScore.y = 240;
-		endScore.font = "normal normal 16px/1.0 monospace";
-		endScore.color = "#FFFFFF";
-		end.addChild(endScore);
-		endReMsg.x = 130;
-		endReMsg.y = 280;
-		endReMsg.font = "normal normal 12px/1.0 monospace";
-		endReMsg.color = "#FFFFFF";
-		end.addChild(endReMsg);
+		//Set BGM
+		var bgm = game.assets['assets/tropicalnojob.mp3'];
 		
 		/*/end
 		end.addEventListener('enterframe', function(){
@@ -82,24 +168,30 @@ window.onload = function() {
 		
 		//rmstart
 		start.addEventListener('enterframe', function(){
+			//press space key(s)
 			if(game.input.s){
 				game.popScene(start);
+								
+				enemies = [];
+				enemiesR = [];
+				enemiesB = [];
 			}
 		});
 		
-		game.score = 0;
-		enemies = [];
-		enemiesR = [];
-		enemiesB = [];
-		
+		//Player
 		Syamu = new Syamu(140, 440);
 		
 		score = new Label(0);
 		score.color = "#000000";
 		score.font = "normal normal 15px/1.0 monospace";
 		
+		//BGM
+		bgm.play();
+		bgm.loop = true;
+		
 		//loop
 		game.rootScene.addEventListener('enterframe', function(){
+			
 			//orig
 			if(game.frame % 10 == 0){
 				var x = Math.random()*(320-32);
@@ -125,6 +217,7 @@ window.onload = function() {
 			}
 
 			score.text = "SCORE: "+game.score;
+			endScore.text = "SCORE: "+game.score;
 			game.rootScene.addChild(score);
 		});
 	}
@@ -164,6 +257,7 @@ window.onload = function() {
 	//enemy
 	var Enemy = enchant.Class.create(enchant.Sprite, {
 		initialize: function(x, y){
+		
 			Sprite.call(this, 32, 32);
 			this.image = game.assets['assets/musyokuorig.png'];
 			this.x = x;
@@ -177,7 +271,7 @@ window.onload = function() {
 				if(this.y > 480 || this.x > 320 || this.x < -this.width || this.y < -this.height){
 					this.remove();
 				}
-				if(Syamu.within(this, 16)){ game.pushScene(end) }
+				if(Syamu.within(this, 16)){ taima(); }
 			});
 			game.rootScene.addChild(this);
 		},
@@ -190,6 +284,10 @@ window.onload = function() {
 	//enemyR
 	var EnemyR = enchant.Class.create(enchant.Sprite, {
 		initialize: function(x, y){
+		
+			//Set BGM
+			var bgm = game.assets['assets/tropicalnojob.mp3'];
+			
 			Sprite.call(this, 32, 32);
 			this.image = game.assets['assets/musyokured.png'];
 			this.x = x;
@@ -203,7 +301,7 @@ window.onload = function() {
 				if(this.y > 480 || this.x > 320 || this.x < -this.width || this.y < -this.height){
 					this.remove();
 				}
-				if(Syamu.within(this, 16)){ game.pushScene(end) }
+				if(Syamu.within(this, 16)){ taima(); }
 			});
 			game.rootScene.addChild(this);
 		},
@@ -216,20 +314,38 @@ window.onload = function() {
 	//enemyB
 	var EnemyB = enchant.Class.create(enchant.Sprite, {
 		initialize: function(x, y){
+			
+			//Set BGM
+			var bgm = game.assets['assets/tropicalnojob.mp3'];
+			
 			Sprite.call(this, 32, 32);
 			this.image = game.assets['assets/musyokublue.png'];
 			this.x = x;
 			this.y = y;
+			
+			var moveflag = Math.floor(Math.random()*2) ;
+			
 			//move
 			this.move = function(){
-				this.y += 3;
+				if(moveflag){
+					if(this.x < 20){
+						moveflag = 0;
+					}
+					this.x -= 5;
+				}else{
+					if(this.x > 300){
+						moveflag = 1;
+					}
+					this.x += 5;
+				}
+				this.y += 5;
 			};
 			this.addEventListener('enterframe', function(){
 				this.move();
 				if(this.y > 480 || this.x > 320 || this.x < -this.width || this.y < -this.height){
 					this.remove();
 				}
-				if(Syamu.within(this, 16)){ game.pushScene(end) }
+				if(Syamu.within(this, 16)){ taima(); }
 			});
 			game.rootScene.addChild(this);
 		},
